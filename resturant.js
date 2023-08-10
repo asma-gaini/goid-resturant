9e5823b1614604255d855736c04548f31b0fb2cd
asma gaini <asma.gaini@yahoo.com>
2023-08-10 13:47:12 +0430

    creat slider plagin in main js and creat new class of plagin in slider.js and adding Next & previous buttons but not complete

---
+++ resturant.js
@@ -0,0 +1,8 @@
+new silder([
+    el : document.querySelector('slide-show'),
+    slideClass : 'slide-show_slider',
+    currentSlider : (slide-show_slider) => {
+        console.log(slide-show_slider);
+    },
+    auto : 3000
+])
